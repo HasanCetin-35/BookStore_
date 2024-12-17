8c6f26a16473deefbe5664eee18d5f5a99eb1cb0
HasanCetin <hasancetinn8636@gmail.com>
2024-12-17 13:30:06 +0300

    new branch

--- MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
+++ MyProject/obj/Debug/net8.0/MyProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+106cb6af1d8e0173bae5530968ef941c8a2fdfb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77825bf6cce7d66808b0a6c2e0fba9fe09665d3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
